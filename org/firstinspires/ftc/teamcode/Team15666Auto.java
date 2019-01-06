584a8c8892c58e9bbb2d4305cfd9cbe5f6bca5d4
M. Dudley <emddudley@gmail.com>
2019-01-06 13:15:35 -0500

    Added standard, crater, and depot autonomous modes

--- org/firstinspires/ftc/teamcode/Team15666Auto.java
+++ org/firstinspires/ftc/teamcode/Team15666Auto.java
@@ -5,24 +5,60 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Standard autonomous OpMode for Team 15666.
  */
 
-@TeleOp(name = "Standard Auto", group = "Linear Opmode")
+@Autonomous(name = "Standard Auto", group = "Linear Opmode")
 public class Team15666Auto extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor right_drive;
+    private DcMotor left_drive;
+    private DcMotor elevator;
+    private Servo claw;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        initSystems();
+        updateTelemetry("Initialized");
 
         waitForStart();
+        runtime.reset();
 
         while (opModeIsActive()) {
+            this.elevator.setPower(0.1);
 
+            if (runtime.seconds() < 2.8) {
+                right_drive.setPower(-1.0);
+                left_drive.setPower(-1.0);
+                claw.setPosition(0.7);
+            } else {
+                right_drive.setPower(0.0);
+                left_drive.setPower(0.0);
+                claw.setPosition(Team15666Devices.ClawOpenPosition);
+            }
+
+            updateTelemetry("Active");
         }
+    }
+
+    private void initSystems() {
+        this.right_drive = hardwareMap.dcMotor.get("right_drive");
+        this.right_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.left_drive = hardwareMap.dcMotor.get("left_drive");
+        this.elevator = hardwareMap.dcMotor.get("arm_1");
+        this.claw = hardwareMap.servo.get("servo_1");
+    }
+
+    private void updateTelemetry(final String status) {
+        this.telemetry.addData("Status", status);
+        this.telemetry.update();
     }
 }
---
+++ org/firstinspires/ftc/teamcode/Team15821AutoCrater.java
@@ -0,0 +1,78 @@
+/**
+ *
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Standard autonomous OpMode for Team 15821.
+ */
+
+@Autonomous(name = "Crater Auto", group = "Linear Opmode")
+public class Team15821AutoCrater extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor right_drive;
+    private DcMotor left_drive;
+    private DcMotor arm_1;
+    private CRServo armServo_1;
+
+    @Override
+    public void runOpMode() {
+        initSystems();
+        updateTelemetry("Initialized");
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            if (runtime.seconds() < 3.6) {
+                arm_1.setPower(1.0);
+                right_drive.setPower(0.0);
+                left_drive.setPower(0.0);
+            } else if (runtime.seconds() < 4.3) {
+                arm_1.setPower(0.0);
+                right_drive.setPower(0.5);
+                left_drive.setPower(-0.5);
+            } else if (runtime.seconds() < 4.8) {
+                arm_1.setPower(0.0);
+                right_drive.setPower(-0.5);
+                left_drive.setPower(-0.5);
+            } else if (runtime.seconds() < 5.4) {
+                arm_1.setPower(0.0);
+                right_drive.setPower(-0.5);
+                left_drive.setPower(0.5);
+            } else if (runtime.seconds() < 7.4) {
+                arm_1.setPower(0.0);
+                right_drive.setPower(-0.5);
+                left_drive.setPower(-0.5);
+            } else {
+                arm_1.setPower(0.0);
+                right_drive.setPower(0.0);
+                left_drive.setPower(0.0);
+            }
+
+            updateTelemetry("Active");
+        }
+    }
+
+    private void initSystems() {
+        this.right_drive = hardwareMap.dcMotor.get("right_drive");
+        this.right_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.left_drive = hardwareMap.dcMotor.get("left_drive");
+        this.arm_1 = hardwareMap.dcMotor.get("arm_1");
+        this.armServo_1 = hardwareMap.crservo.get("armServo_1");
+    }
+
+    private void updateTelemetry(final String status) {
+        this.telemetry.addData("Status", status);
+        this.telemetry.update();
+    }
+}
