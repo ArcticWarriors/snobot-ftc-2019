05935a9efd8526830e037570e4a047f8bb7e8a35
M. Dudley <emddudley@gmail.com>
2019-01-11 18:31:54 -0500

    Updated timing and pathing

--- org/firstinspires/ftc/teamcode/Team15821AutoDepot.java
+++ org/firstinspires/ftc/teamcode/Team15821AutoDepot.java
@@ -32,53 +32,75 @@ public class Team15821AutoDepot extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        final float lowerLanderTime = 1.8f;
+        final float rotateOffLanderTime = lowerLanderTime + 1.0f;
+        final float moveAwayFromLanderTime = rotateOffLanderTime + 0.35f;
+        final float lowerArmTime = moveAwayFromLanderTime + 1.0f;
+        final float adjustDrivingAngleTime = lowerArmTime + 1.25f;
+        final float driveToDepotTime = adjustDrivingAngleTime + 6.7f;
+        final float ejectMarkerTime = driveToDepotTime + 2.0f;
+        final float turnToCraterTime = ejectMarkerTime + 1.2f;
+        final float driveToCraterTime = turnToCraterTime + 3.4f;
+        final float driveCorrectionToCraterTime = driveToCraterTime + 2.6f;
+
         while (opModeIsActive()) {
-            if (runtime.seconds() < 3.6) {
+            if (runtime.seconds() < lowerLanderTime) {
+                // Lower from lander
                 arm_1.setPower(1.0);
+                armServo_1.setPower(0.0);
                 right_drive.setPower(0.0);
                 left_drive.setPower(0.0);
-            } else if (runtime.seconds() < 4.3) {
+            } else if (runtime.seconds() < rotateOffLanderTime) {
+                // Rotate off lander hook
                 arm_1.setPower(0.0);
                 right_drive.setPower(0.5);
                 left_drive.setPower(-0.5);
-            } else if (runtime.seconds() < 4.8) {
+            } else if (runtime.seconds() < moveAwayFromLanderTime) {
+                // Move away from lander, 0.8
                 arm_1.setPower(0.0);
                 right_drive.setPower(-0.5);
                 left_drive.setPower(-0.5);
-            } else if (runtime.seconds() < 6.4) {
+            } else if (runtime.seconds() < lowerArmTime) {
+                // Lower arm, 1.6
                 arm_1.setPower(-1.0);
                 right_drive.setPower(0.0);
                 left_drive.setPower(0.0);
-            } else if (runtime.seconds() < 7.5) {
+            } else if (runtime.seconds() < adjustDrivingAngleTime) {
+                // Adjust driving angle, 1.1
                 arm_1.setPower(0.0);
                 right_drive.setPower(-0.5);
                 left_drive.setPower(0.5);
-            } else if (runtime.seconds() < 13.0) {
+            } else if (runtime.seconds() < driveToDepotTime) {
+                // Drive to depot, 5.9
                 arm_1.setPower(0.0);
                 right_drive.setPower(-0.5);
                 left_drive.setPower(-0.5);
-            } else if (runtime.seconds() < 13.4) {
+            } else if (runtime.seconds() < ejectMarkerTime) {
+                // Stop and eject the marker, 0.4
                 arm_1.setPower(0.0);
-                right_drive.setPower(-1.0);
-                left_drive.setPower(-1.0);
-            } else if (runtime.seconds() < 13.6) {
-                arm_1.setPower(0.0);
-                right_drive.setPower(1.0);
-                left_drive.setPower(1.0);
-            } else if (runtime.seconds() < 14.1) {
-                arm_1.setPower(0.0);
+                armServo_1.setPower(-0.7);
                 right_drive.setPower(0.0);
                 left_drive.setPower(0.0);
-            } else if (runtime.seconds() < 17.4) {
+            } else if (runtime.seconds() < turnToCraterTime) {
+                // Turn to crater, 3
                 arm_1.setPower(0.0);
+                armServo_1.setPower(0.0);
                 right_drive.setPower(0.5);
                 left_drive.setPower(-0.5);
-            } else if (runtime.seconds() < 23.9) {
+            } else if (runtime.seconds() < driveToCraterTime) {
+                // Drive to crater, 6.5
                 arm_1.setPower(0.0);
                 right_drive.setPower(0.7);
                 left_drive.setPower(0.7);
-            } else {
+            } else if (runtime.seconds() < driveCorrectionToCraterTime) {
+                // Drive to crater, correct angle
                 arm_1.setPower(0.0);
+                right_drive.setPower(0.5);
+                left_drive.setPower(0.7);
+            } else {
+                // Stop
+                arm_1.setPower(0.0);
+                armServo_1.setPower(0.0);
                 right_drive.setPower(0.0);
                 left_drive.setPower(0.0);
             }
